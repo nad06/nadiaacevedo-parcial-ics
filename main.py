7c5b70cabfec669f88bf2b99fa3abaae99c947f5
Nadia Acevedo <nadiamicaela06@ca.frre.utn.edu.ar>
2026-06-01 11:36:56 -0300

    agrego puerto pra render

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from saludos import saludar
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,7 +9,7 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(saludar("Nadia").encode())
 
 if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 10000), Handler)
-    print("Servidor corriendo...")
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    print(f"Servidor corriendo en puerto {port}...")
     server.serve_forever()
-
