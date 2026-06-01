0e7ba534be0d273d12ffdbf9a0a8460c0452f5a3
Nadia Acevedo <nadiamicaela06@ca.frre.utn.edu.ar>
2026-06-01 11:33:41 -0300

    correción en main.py para deploy en render

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from saludos import saludar
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(saludar("Nadia").encode())
 
 if __name__ == "__main__":
-    print(saludar("Nadia"))
+    server = HTTPServer(("0.0.0.0", 10000), Handler)
+    print("Servidor corriendo...")
+    server.serve_forever()
+
